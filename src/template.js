4adadd724f44975ea74fa29704b96b043adfb63a
dave <davidpascual200@gmail.com>
2022-08-28 14:18:56 -0700

    fixed github link

--- src/template.js
+++ src/template.js
@@ -29,7 +29,7 @@ const engineerCard = (engineer) => {
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${engineer.getId()}</li>
                 <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                <li class="list-group-item">Github: ${engineer.getGithub()}</li>
+                <li class="list-group-item">Github: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
             </ul>
         </div>
     </div>`
